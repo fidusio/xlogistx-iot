646fd534de29959b8acdd297d00985a471bb3535
javaconsigliere <javaconsigliere@gmail.com>
2018-03-30 20:28:56 -0700

    update

--- src/main/java/io/xlogistx/iot/gpio/GPIOTools.java
+++ src/main/java/io/xlogistx/iot/gpio/GPIOTools.java
@@ -13,6 +13,7 @@ import org.zoxweb.shared.util.SharedUtil;
 
 import com.pi4j.io.gpio.GpioController;
 import com.pi4j.io.gpio.GpioFactory;
+import com.pi4j.io.gpio.GpioPin;
 import com.pi4j.io.gpio.GpioPinDigitalOutput;
 import com.pi4j.io.gpio.Pin;
 import com.pi4j.io.gpio.PinState;
@@ -53,7 +54,14 @@ public class GPIOTools
 		
 		log.info(SharedUtil.toCanonicalID(',', pin, state, persist, durationInMillis));
 		
+		GpioPin gpioPin = SINGLETON.getGpioController().getProvisionedPin(pin);
+		if(gpioPin != null)
+		{
+			SINGLETON.getGpioController().unprovisionPin(gpioPin);
+		}
+		
 		GpioPinDigitalOutput output = SINGLETON.getGpioController().provisionDigitalOutputPin(pin, state);
+
 		if (persist)
 			output.setShutdownOptions(false, state);
 		output.setState(state);
