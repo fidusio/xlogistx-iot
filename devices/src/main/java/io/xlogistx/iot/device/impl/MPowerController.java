8a8db6f60000a3d92db766dd6125b9a0c103f604
javaconsigliere <javaconsigliere@gmail.com>
2019-08-21 14:02:22 -0700

    Made the MPowerController to handle multiple controller

--- devices/src/main/java/io/xlogistx/iot/device/impl/MPowerController.java
+++ devices/src/main/java/io/xlogistx/iot/device/impl/MPowerController.java
@@ -1,5 +1,6 @@
 package io.xlogistx.iot.device.impl;
 
+import java.util.regex.Pattern;
 import org.zoxweb.shared.net.InetSocketAddressDAO;
 import org.zoxweb.shared.util.Const.Bool;
 import org.zoxweb.shared.util.NVPair;
@@ -11,7 +12,7 @@ public class MPowerController
     try
     {
       int i = 0;
-      String url = args[i++];
+      String urls[] = args[i++].split(Pattern.quote(","));
       String user = args[i++];
       String passwd = args[i++];
       int port = Integer.parseInt(args[i++]);
@@ -21,7 +22,7 @@ public class MPowerController
       {
         proxy = new InetSocketAddressDAO(args[i++]);
       }
-      long ts = System.currentTimeMillis();
+
 
 
 
@@ -31,20 +32,29 @@ public class MPowerController
       //System.exit(0);
 
 
-      NVPair sessionCookie = UBNTEqpt.loginCookie(url, user, passwd, proxy);
+      for (String url : urls) {
+        try {
+          long ts = System.currentTimeMillis();
+          NVPair sessionCookie = UBNTEqpt.loginCookie(url, user, passwd, proxy);
 
-      //System.out.println(UBNTMPower.reboot(url, sessionCookie));
+          //System.out.println(UBNTMPower.reboot(url, sessionCookie));
 
-      //System.exit(-1);
+          //System.exit(-1);
 
-      String response = null;
+          String response = null;
 
-      response = UBNTEqpt.controlPort(url, sessionCookie, port, on, proxy);
-      System.out.println(response);
-      response = UBNTEqpt.getSensorsStatus(url, sessionCookie);
-      System.out.println(response);
-      ts = System.currentTimeMillis() - ts;
-      System.out.println( response + "\nin " + ts + " millis.");
+          response = UBNTEqpt.controlPort(url, sessionCookie, port, on, proxy);
+          System.out.println(response);
+          response = UBNTEqpt.getSensorsStatus(url, sessionCookie);
+          System.out.println(response);
+          ts = System.currentTimeMillis() - ts;
+          System.out.println(response + "\nin " + ts + " millis.");
+        }
+        catch (Exception e)
+        {
+          e.printStackTrace();
+        }
+      }
       //System.out.println( System.currentTimeMillis() + " millis.");
       //System.out.println( UBNTMPower.getSensorsStatus(url, cookie));
 
