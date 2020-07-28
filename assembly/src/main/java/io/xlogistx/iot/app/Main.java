2bb87e0ee8928f33202ad2619b72d22b978ac840
javaconsigliere <javaconsigliere@gmail.com>
2020-07-27 17:20:08 -0700

    updat

---
+++ assembly/src/main/java/io/xlogistx/iot/app/Main.java
@@ -0,0 +1,53 @@
+package io.xlogistx.iot.app;
+
+import io.xlogistx.http.HTTPServerCreator;
+import io.xlogistx.iot.gpio.GPIOFlowProcessor;
+import io.xlogistx.iot.gpio.PinStateMonitorConfig;
+import org.zoxweb.server.io.IOUtil;
+import org.zoxweb.server.task.TaskUtil;
+import org.zoxweb.server.util.GSONUtil;
+import org.zoxweb.shared.http.HTTPServerConfig;
+import org.zoxweb.shared.util.ParamUtil;
+
+import java.io.File;
+import java.util.logging.Logger;
+
+public class Main
+{
+    private final static Logger log = Logger.getLogger(Main.class.getName());
+    public static void main(String ...args)
+    {
+        try
+        {
+            ParamUtil.ParamMap params = ParamUtil.parse("-", args);
+            String wsConfig = params.stringValue("-wsc", true);
+            String flowConfig = params.stringValue("-fc", true);
+
+
+
+            if(wsConfig != null)
+            {
+                File file = IOUtil.locateFile(wsConfig);
+                HTTPServerConfig hsc = GSONUtil.fromJSON(IOUtil.inputStreamToString(file), HTTPServerConfig.class);
+                log.info("HTTPServerConfig:" + hsc);
+                log.info("WebServer json:" + hsc.getConnectionConfigs());
+                HTTPServerCreator httpServerCreator = new HTTPServerCreator();
+                httpServerCreator.setAppConfig(hsc);
+                httpServerCreator.createApp();
+            }
+            if(flowConfig != null)
+            {
+                File file = IOUtil.locateFile(flowConfig);
+                String jsonFC = IOUtil.inputStreamToString(file);
+                log.info("" + jsonFC);
+                PinStateMonitorConfig pinStateMonitorConfig = GSONUtil.DEFAULT_GSON.fromJson(jsonFC, PinStateMonitorConfig.class);
+                new GPIOFlowProcessor(pinStateMonitorConfig, TaskUtil.getDefaultTaskScheduler()).init();
+            }
+
+        }
+        catch(Exception e)
+        {
+            e.printStackTrace();
+        }
+    }
+}
