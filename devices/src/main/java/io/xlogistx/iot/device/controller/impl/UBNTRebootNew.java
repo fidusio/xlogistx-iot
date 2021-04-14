d351b37c4bed9ac74cea439109ee2f58f64676c5
mnael <javaconsigliere@gmail.com>
2021-04-13 17:29:02 -0700

    update

--- devices/src/main/java/io/xlogistx/iot/device/controller/impl/UBNTRebootNew.java
+++ devices/src/main/java/io/xlogistx/iot/device/controller/impl/UBNTRebootNew.java
@@ -1,5 +1,6 @@
 package io.xlogistx.iot.device.controller.impl;
 
+import io.xlogistx.common.task.RunnableProperties;
 import org.zoxweb.server.http.HTTPCall;
 import org.zoxweb.server.util.GSONUtil;
 import org.zoxweb.shared.http.HTTPMessageConfig;
@@ -10,9 +11,58 @@ import org.zoxweb.shared.util.NVGenericMap;
 import org.zoxweb.shared.util.NVPair;
 import org.zoxweb.shared.util.SharedStringUtil;
 
-import java.util.Locale;
 
-public class UBNTRebootNew {
+import java.util.logging.Logger;
+
+
+public class UBNTRebootNew
+  extends RunnableProperties
+{
+    private static final Logger log = Logger.getLogger(UBNTRebootNew.class.getName());
+
+    public UBNTRebootNew(){}
+    public UBNTRebootNew(String host, String username, String password)
+    {   host = host.toLowerCase();
+        if(!host.startsWith("https://"))
+            host = "https://" + host;
+
+        setProperties(new NVGenericMap());
+        getProperties().add("host", host);
+        getProperties().add("username", username);
+        getProperties().add("password", password);
+
+    }
+
+    public void run()
+    {
+        try
+        {
+            HTTPMessageConfigInterface hmci = HTTPMessageConfig.createAndInit(getProperties().getValue("host"), "/api/v1.0/user/login", HTTPMethod.POST, false);
+            hmci.setContentType("application/json");
+            NVGenericMap nvgm = new NVGenericMap();
+            nvgm.add(getProperties().get("username"));
+            nvgm.add(getProperties().get("password"));
+
+            hmci.setContent(GSONUtil.toJSONGenericMap(nvgm, false, false, false));
+            HTTPCall hc = new HTTPCall(hmci);
+            HTTPResponseData rd = hc.sendRequest();
+
+
+            String authToken = rd.getResponseHeaders().get("x-auth-token").get(0);
+            hmci = HTTPMessageConfig.createAndInit(getProperties().getValue("host"), "/api/v1.0/system/reboot", HTTPMethod.POST, false);
+            hmci.setContentType("application/json");
+            hmci.getHeaderParameters().add(new NVPair("x-auth-token", authToken));
+
+            hc = new HTTPCall(hmci);
+            rd = hc.sendRequest();
+            log.info("Device: " + getProperties().getValue("host") + ", " + SharedStringUtil.toString(rd.getData()));
+        }
+        catch(Exception e)
+        {
+            e.printStackTrace();
+        }
+    }
+
     public static void main(String ...args)
     {
         try
@@ -20,36 +70,16 @@ public class UBNTRebootNew {
             int index = 0;
             String username = args[index++];
             String password = args[index++];
-            for (;index < args.length;) {
-                String url = args[index++].toLowerCase();
-                if(!url.startsWith("https://"))
-                    url = "https://" + url;
-
-                HTTPMessageConfigInterface hmci = HTTPMessageConfig.createAndInit(url, "/api/v1.0/user/login", HTTPMethod.POST, false);
-                hmci.setContentType("application/json");
-                NVGenericMap nvgm = new NVGenericMap();
-                nvgm.add("username", username);
-                nvgm.add("password", password);
-                hmci.setContent(GSONUtil.toJSONGenericMap(nvgm, false, false, false));
-                HTTPCall hc = new HTTPCall(hmci);
-                HTTPResponseData rd = hc.sendRequest();
-
-
-                String authToken = rd.getResponseHeaders().get("x-auth-token").get(0);
-                hmci = HTTPMessageConfig.createAndInit(url, "/api/v1.0/system/reboot", HTTPMethod.POST, false);
-                hmci.setContentType("application/json");
-                hmci.getHeaderParameters().add(new NVPair("x-auth-token", authToken));
-
-                hc = new HTTPCall(hmci);
-                rd = hc.sendRequest();
-
-                System.out.println("Device: " + url + " " + SharedStringUtil.toString(rd.getData()));
+            for (;index < args.length;)
+            {
+                UBNTRebootNew ubntSwitch = new UBNTRebootNew(args[index++], username, password);
+                ubntSwitch.run();
             }
-
         }
         catch(Exception e)
         {
             e.printStackTrace();
+            System.err.println("usage: username password [hosts...]");
         }
     }
 }
