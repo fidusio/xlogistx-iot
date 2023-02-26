41607d1c3272b0026fa71331156d51cab8b4fb0f
javaconsgileire <javaconsigliere@gmail.com>
2023-02-25 23:45:47 -0800

    Added ArtemisSM

---
+++ common/src/main/java/io/xlogistx/iot/mqtt/ArtemisSM.java
@@ -0,0 +1,81 @@
+package io.xlogistx.iot.mqtt;
+
+import org.apache.activemq.artemis.core.security.CheckType;
+import org.apache.activemq.artemis.core.security.Role;
+import org.apache.activemq.artemis.spi.core.protocol.RemotingConnection;
+import org.apache.activemq.artemis.spi.core.security.ActiveMQSecurityManager;
+import org.apache.activemq.artemis.spi.core.security.ActiveMQSecurityManager5;
+import org.zoxweb.server.http.HTTPCall;
+import org.zoxweb.shared.http.HTTPMessageConfig;
+import org.zoxweb.shared.http.HTTPMessageConfigInterface;
+import org.zoxweb.shared.http.HTTPMethod;
+import org.zoxweb.shared.http.HTTPMimeType;
+import org.zoxweb.shared.util.SharedUtil;
+
+import javax.security.auth.Subject;
+import java.util.Map;
+import java.util.Set;
+import java.util.logging.Logger;
+
+public class ArtemisSM
+    implements ActiveMQSecurityManager5
+{
+
+    private String loginURL;
+    private static final Logger log = Logger.getLogger(ArtemisSM.class.getName());
+    @Override
+    public Subject authenticate(String user, String password, RemotingConnection remotingConnection, String securityDomain) {
+        try
+        {
+            log.info(SharedUtil.toCanonicalID(':', user));
+            HTTPMessageConfigInterface hmci = HTTPMessageConfig.createAndInit(loginURL, null, HTTPMethod.GET);
+            hmci.setUser(user);
+            hmci.setPassword(password);
+            hmci.setContentType(HTTPMimeType.APPLICATION_JSON);
+            HTTPCall.send(hmci);
+            return new Subject();
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+
+        return null;
+
+        //true, new JSPrincipal(user).asSet(), null, null);
+    }
+
+    @Override
+    public boolean authorize(Subject subject, Set<Role> roles, CheckType checkType, String address) {
+
+        return true;
+    }
+
+    @Override
+    public String getDomain() {
+        return ActiveMQSecurityManager5.super.getDomain();
+    }
+
+    @Override
+    public boolean validateUser(String user, String password) {
+        log.info(user + " " + password);
+        return true;
+    }
+
+    @Override
+    public boolean validateUserAndRole(String s, String s1, Set<Role> set, CheckType checkType) {
+        return true;
+    }
+
+    @Override
+    public ActiveMQSecurityManager init(Map<String, String> properties) {
+        log.info(""+properties);
+        if(properties != null)
+        {
+            loginURL = properties.get("url");
+            log.info("login url: " + loginURL);
+        }
+        return this;
+        //return ActiveMQSecurityManager5.super.init(properties);
+    }
+}
