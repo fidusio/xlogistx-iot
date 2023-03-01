0a3d83c035a8728d449fc175a046568cb61353e5
javaconsgileire <javaconsigliere@gmail.com>
2023-02-28 20:02:12 -0800

    updated the artemis security manager

--- common/src/main/java/io/xlogistx/iot/mqtt/ArtemisSM.java
+++ common/src/main/java/io/xlogistx/iot/mqtt/ArtemisSM.java
@@ -6,6 +6,7 @@ import org.apache.activemq.artemis.spi.core.protocol.RemotingConnection;
 import org.apache.activemq.artemis.spi.core.security.ActiveMQSecurityManager;
 import org.apache.activemq.artemis.spi.core.security.ActiveMQSecurityManager5;
 import org.zoxweb.server.http.HTTPCall;
+import org.zoxweb.server.security.JSPrincipal;
 import org.zoxweb.shared.http.HTTPMessageConfig;
 import org.zoxweb.shared.http.HTTPMessageConfigInterface;
 import org.zoxweb.shared.http.HTTPMethod;
@@ -24,23 +25,29 @@ public class ArtemisSM
     private String loginURL;
     private static final Logger log = Logger.getLogger(ArtemisSM.class.getName());
     @Override
-    public Subject authenticate(String user, String password, RemotingConnection remotingConnection, String securityDomain) {
+    public Subject authenticate(String user, String password, RemotingConnection remotingConnection, String securityDomain)
+    {
+        Subject ret = null;
         try
         {
-            log.info(SharedUtil.toCanonicalID(':', user));
+            log.info(Thread.currentThread() + ":" + SharedUtil.toCanonicalID(':', user,remotingConnection));
+            log.info("RemoteConnection id: " + remotingConnection.getID());
             HTTPMessageConfigInterface hmci = HTTPMessageConfig.createAndInit(loginURL, null, HTTPMethod.GET);
             hmci.setUser(user);
             hmci.setPassword(password);
             hmci.setContentType(HTTPMimeType.APPLICATION_JSON);
             HTTPCall.send(hmci);
-            return new Subject();
+            ret = new Subject();
+            ret.getPrincipals().add(new JSPrincipal(user));
+
+            remotingConnection.setSubject(ret);;
         }
         catch (Exception e)
         {
             e.printStackTrace();
         }
 
-        return null;
+        return ret;
 
         //true, new JSPrincipal(user).asSet(), null, null);
     }
