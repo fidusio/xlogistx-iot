2a303e4ffec883c98fbcb6ec4132bb604d548e08
javaconsgileire <javaconsigliere@gmail.com>
2024-08-10 12:19:02 -0700

    Adde the AtmelDeviceFactory

---
+++ devices/src/main/java/io/xlogistx/iot/device/shared/uc/atmel/AtmelDeviceFactory.java
@@ -0,0 +1,15 @@
+package io.xlogistx.iot.device.shared.uc.atmel;
+
+public class AtmelDeviceFactory
+{
+
+    public enum AtmelUC{
+        ATTINY_84,
+        ATTINY_85,
+        ATMEGA_328,
+    }
+
+
+
+
+}
